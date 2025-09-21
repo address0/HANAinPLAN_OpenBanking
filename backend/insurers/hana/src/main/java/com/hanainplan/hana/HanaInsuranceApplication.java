427444054af78fbf0a00c4aced494df3eb8f29cb
address0 <soyong6624@gmail.com>
2025-09-21 16:40:57 +0900

    [BE]feat: 각 보험사별 서버(하나, 한화, 삼성) 구축

---
+++ backend/insurers/hana/src/main/java/com/hanainplan/hana/HanaInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.hanainplan.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaInsuranceApplication.class, args);
+    }
+
+}
---
+++ backend/insurers/hanhwa/src/main/java/com/hanainplan/hanhwa/HanhwaInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.hanainplan.hanhwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanhwaInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanhwaInsuranceApplication.class, args);
+    }
+
+}
